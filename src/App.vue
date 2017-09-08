852e1c7278d1f65176e00a4cde77bceb576130f4
Gabriel Dunn <gabe.dunn@shaw.ca>
2017-09-07 22:28:51 -0600

    added default font stack

--- src/App.vue
+++ src/App.vue
@@ -19,5 +19,21 @@
 </script>
 
 <style lang="scss">
-  // SCSS styles
+  // Font imports
+  @import url('https://fonts.googleapis.com/css?family=Roboto:300,400|Source+Sans+Pro:300,400');
+
+  // Variables
+  :root {
+    --font-stack: 'Roboto', BlinkMacSystemFont, -apple-system, "Oxygen", "Ubuntu", "Fira Sans", "Helvetica", "Arial", sans-serif;
+    .light {
+      // Light theme variables
+    }
+    .dark {
+      // Dark theme variables
+    }
+  }
+
+  html {
+    font-family: var(--font-stack);
+  }
 </style>
