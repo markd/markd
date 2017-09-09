263891e64bebe6671dcb82ea6e6109370a89e05b
Gabriel Dunn <gabe.dunn@shaw.ca>
2017-09-09 14:35:56 -0600

    merge pull main repo to fork (#1)
    * Added CI configuration for Travis (#4)
    
    * Add unit testing (#5)
    
    * Added CI configuration for Travis
    
    * Added packeges to package.json and setup karma with PhantomJS
    
    * Updated Travis config to run tests
    
    * Replaced Karma in favor of AVA
    
    * Adapted babel config (added AVA presets)
    
    * Added example test
    
    * Removed Karma config
    
    * made .babelrc support uglify
    
    * added eslint vue rules
    
    * added custom eslint vue rules
    
    * made dependencies not use latest in package.json
    
    * moved scss to scss files for easier layouts
    
    * added custom eslint vue rules (#6)
    
    * added build status & license badges to readme

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,48 @@
 module.exports = {
   root: true,
-  parser: 'babel-eslint',
   parserOptions: {
+    parser: 'babel-eslint',
     sourceType: 'module'
   },
   env: {
-    browser: true,
+    browser: true
   },
-  extends: 'standard',
+  extends: [
+    'standard',
+    'plugin:vue/recommended'
+  ],
   plugins: [
     'html'
   ],
   'rules': {
     'arrow-parens': 0,
     'generator-star-spacing': 0,
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'vue/return-in-computed-property': 2,
+    'vue/no-dupe-keys': 2,
+    'vue/require-render-return': 2,
+    'vue/no-duplicate-attributes': 2,
+    'vue/attribute-hyphenation': 2,
+    'vue/html-quotes': 2,
+    'vue/no-multi-spaces': 2,
+    'vue/order-in-components': [
+      2, {
+        order: [
+          'el',
+          ['name', 'delimiters', 'functional', 'model'],
+          ['components', 'directives', 'filters'],
+          ['parent', 'mixins', 'extends', 'provide', 'inject'],
+          'template',
+          'props',
+          'propsData',
+          'data',
+          'computed',
+          'watch',
+          'LIFECYCLE_HOOKS',
+          'methods',
+          'render',
+          'renderError'
+        ]
+      }]
   }
 }
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,16 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    name: 'Sidebar',
+    components: {},
+    props: ['title'],
+    data () {
+      return {
+        //
+      }
+    }
+  }
+</script>
