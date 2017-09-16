40bd751159abe359380bbd65cabb9cbf54657573
Gabriel Dunn <gabe.dunn@shaw.ca>
2017-09-15 22:04:19 -0600

    made it easier to use .env files

--- config/index.js
+++ config/index.js
@@ -24,7 +24,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: process.env.PORT,
+    port: process.env.PORT || '8000',
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
