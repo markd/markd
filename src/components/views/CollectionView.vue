fb52402ff994a3b77f4f6eb33c19a30f9eb74ea3
lost.design <info@lost.design>
2018-07-16 01:34:37 +0200

    now fetching all items, independend from collection/tags (needs to be fixed)

--- src/components/views/CollectionView.vue
+++ src/components/views/CollectionView.vue
@@ -1,11 +1,29 @@
 <template>
-  <span>Showing CollectionView for id {{ $route.params.id }}</span>
+  <div>
+    <span>Showing CollectionView for id {{ $route.params.id }}</span>
+    <br>
+    <template v-for="item in items">
+      <a :href="item.url" :key="item.key" target="_blank">{{item.name}}</a>
+    </template>
+  </div>
 </template>
 
 <script>
   export default {
     name: 'CollectionView',
     computed: {
+      items() {
+        return this.$store.getters.getItems(this.$route.params.id)
+      }
     }
   }
 </script>
+
+<style lang="scss" scoped>
+  a {
+    display:block;
+    text-decoration: none;
+    margin-bottom: 1rem
+  }
+</style>
+
